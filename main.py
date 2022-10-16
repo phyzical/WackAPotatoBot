6659965b8b8fe5964fcb9f26da9d935abbda9747
phyzical <5182053+phyzical@users.noreply.github.com>
2022-10-16 14:22:47 +0800

    added logic to detect the game is over and sleep till next game

--- main.py
+++ main.py
@@ -2,48 +2,26 @@ from os import listdir
 from os.path import basename
 import cv2
 import numpy as np
-from functools import reduce
 import pyautogui
 import time
-import random
 import mss
 
 imageDir = 'images'
-debug = True
+debug = False
 baseX = 130
 baseY = 40
 extraWidth = 900
 extraHeight = 850
-
-
-def filterFiles(file):
-    return file != imageDir+"/.DS_Store"
-
-
-refImages = filter(
-    filterFiles, [imageDir+"/" + s for s in listdir(imageDir)])
+refImages = [imageDir+"/" + s for s in listdir(imageDir)]
 
 
 def initiliseImages(imageFile):
     imageFileName = basename(imageFile).split(".", 1)[0]
     image = cv2.imread(imageFile)
-    # # convert the images to grayscale
-    # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return {"image": image, "type": imageFileName}
 
 
 imageTypes = list(map(initiliseImages, refImages))
-
-
-def removeDuplicates(list, item):
-    unique = 1
-    errorVariancePixels = 10
-    for listItem in list:
-        unique = unique and (
-            abs(listItem["x"] - item["x"]) > errorVariancePixels or abs(listItem["y"] - item["y"]) > errorVariancePixels)
-    if unique == 1:
-        list.append(item)
-    return list
 
 
 def findImages(i, types):
@@ -76,12 +54,10 @@ def findImages(i, types):
         print("time taken for read: " + str(end - start))
         totalTime += end - start
 
-    groupedImages = {}
     for tileType in findRefImageByTypes(types):
         typeImage = tileType["image"]
         type = tileType["type"]
         w, h = typeImage.shape[:-1]
-        tiles = []
         if debug:
             start = time.time()
         res = cv2.matchTemplate(img_rgb, typeImage, cv2.TM_CCOEFF_NORMED)
@@ -98,31 +74,23 @@ def findImages(i, types):
             end = time.time()
             print("time taken for np:" + str(end - start))
             totalTime += end - start
-
-        for pt in zip(*loc[::-1]):  # Switch collmns and rows
+        matches = list(zip(*loc[::-1]))
+        if len(matches) > 0:
+            pt = matches[0]  # Switch collmns and rows
             # xy of center point for moving
             x = pt[0] + (w/2)
             y = pt[1] + (h/2)
-            tiles.append({"x": x, "y": y, "pt": pt})
-
-        groupedImagesByType = reduce(removeDuplicates, tiles, [])
-        if len(groupedImagesByType) > 0:
-            groupedImages[type] = groupedImagesByType
+            tile = {"x": x, "y": y}
             if debug:
-                for tile in groupedImages[type]:
-                    pt = tile["pt"]
-                    cv2.rectangle(img_rgb, pt,
-                                  (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-                    cv2.putText(img_rgb, type,
-                                (pt[0] + w, pt[1] + h),  cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+                cv2.rectangle(img_rgb, pt,
+                              (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+                cv2.putText(img_rgb, type,
+                            (pt[0] + w, pt[1] + h),  cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+                cv2.imwrite('tests/result-'+i+'.png', img_rgb)
+                print("time taken: " + str(totalTime))
+            return tile
     if debug:
-        cv2.imwrite('tests/result-'+i+'.png', img_rgb)
         print("time taken: " + str(totalTime))
-    # we do this as this game only needs 1 not groups
-    group = list(groupedImages.values())
-    if len(group) > 0:
-        return group[0][0]
-    # return list(groupedImages.values())
 
 
 def findRefImageByTypes(types):
@@ -142,14 +110,24 @@ def start():
         print(start)
     click(start)
     i = 2
-    while i < 180:
+    running = True
+    while running:
+        # TODO: if we have combo shields left and at max combo, then get rid of reds
         potato = findImages(str(i), ["green", "yellow"])
         if potato:
             if debug:
                 print("Clicking Potato:")
                 print(potato)
             click(potato)
-        i = + 1
+        i = i + 1
+        if i % 100 == 0:
+            print("checking if the game finished")
+            timer = findImages(str(i), ["timer"])
+            if debug:
+                print("Found Game Finished timer:")
+                print(timer)
+            if timer:
+                running = False
 
 
 while True:
@@ -157,4 +135,8 @@ while True:
     if debug:
         print(startText)
     if startText:
+        print("Starting game")
         start()
+    # TODO: before sleeping we could go and auto destroy equipment?
+    print("Sleeping for 5 minutes")
+    time.sleep(60*5)
